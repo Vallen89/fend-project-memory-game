0e1bed1322534a0b63b13508f5f6fe75b73b2e5d
Ali Tübek <tuebek_ali@gmx.net>
2018-02-11 20:39:06 +0100

    README Updated

--- README.md
+++ README.md
@@ -18,3 +18,16 @@ For specific, detailed instructions, look at the project instructions in the [Ud
 This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
 
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+## The Game
+
+The Goal of the Memory Game is to find all the matching Cards in the Deck.
+
+Click a card to show its Symbol. Click a second card to find its matching Pair.
+If the Symbols do not match both cards will be flipped Back. If they match both Cards will stay open and change color to green.
+
+You win the Game by matching all the Cards. When you win the Game a screen will show you how many attempts you needed and how long it took you to finish. You can start another Game by clicking "Restart"
+There will also be a Star Count to show you how well you played that round.
+For less than 12 moves you get a 3 Star Rating, for 13 to 16 moves a 2 Star Score, 17 to 20 a 1 Star Score and for more than 21 moves a 0 Star Score.
+
+You can also restart the game by clicking the symbol in the upper right corner.
--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /* a list that holds all cards */
- var cardlist = ["diamondOne", "diamondTwo", "planeOne", "planeTwo", "anchorOne", "anchorTwo", "boltOne", "boltTwo", "cubeOne", "cubeTwo", "leafOne", "leafTwo", "bycicleOne", "bycicleTwo", "bombOne", "bombTwo"];
- cardlist = shuffle(cardlist);
+var cardlist = ["diamondOne", "diamondTwo", "planeOne", "planeTwo", "anchorOne", "anchorTwo", "boltOne", "boltTwo", "cubeOne", "cubeTwo", "leafOne", "leafTwo", "bycicleOne", "bycicleTwo", "bombOne", "bombTwo"];
+cardlist = shuffle(cardlist);
 
 /*
  * Display the cards on the page
@@ -16,55 +16,48 @@ sets moves to 0.
 sets the star Count back by changing the inner HTML of every li Element.
  */
 
- function löschen() {
-   let deckEvent = document.querySelector('.deck');
-   deckEvent.addEventListener("click", time);
-   let move = document.querySelector('.moves');
-   move.innerText = "0";
-   document.getElementById("time").innerHTML = "0s";
-   clearTimeout(myVar);
-   let stars = document.querySelector('.stars');
-   stars.children[0].innerHTML = '<i class="fa fa-star"></i>';
-   stars.children[1].innerHTML = '<i class="fa fa-star"></i>';
-   stars.children[2].innerHTML = '<i class="fa fa-star"></i>';
-let allCards = document.querySelectorAll(".card");
-       for (y = 0; y < allCards.length; y++) {
-         let everyCard = allCards[y];
-          let changeCards = cardlist[y];
-          everyCard.className = "card";
- everyCard.firstElementChild.remove();
-  if (changeCards === "diamondOne" || changeCards === "diamondTwo") {
-    everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-diamond"></i>');
-  }
-  else if (changeCards === "planeOne" || changeCards === "planeTwo") {
-    everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-paper-plane-o"></i>');
-  }
-  else if (changeCards === "anchorOne" || changeCards === "anchorTwo"){
-    everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-anchor"></i>');
-  }
-  else if (changeCards === "boltOne" || changeCards === "boltTwo"){
-    everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-bolt"></i>');
-  }
-  else if (changeCards === "cubeOne" || changeCards === "cubeTwo"){
-    everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-cube"></i>');
-  }
-  else if (changeCards === "leafOne" || changeCards === "leafTwo"){
-    everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-leaf"></i>');
-  }
-  else if (changeCards === "bycicleOne" || changeCards === "bycicleTwo"){
-    everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-bicycle"></i>');
-  }
-  else if (changeCards === "bombOne" || changeCards === "bombTwo") {
-    everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-bomb"></i>');
-  }
-  else {};
- }
- }
- löschen();
+function löschen() {
+    let deckEvent = document.querySelector('.deck');
+    deckEvent.addEventListener("click", time);
+    let move = document.querySelector('.moves');
+    move.innerText = "0";
+    document.getElementById("time").innerHTML = "0s";
+    clearTimeout(myVar);
+    let stars = document.querySelector('.stars');
+    stars.children[0].innerHTML = '<i class="fa fa-star"></i>';
+    stars.children[1].innerHTML = '<i class="fa fa-star"></i>';
+    stars.children[2].innerHTML = '<i class="fa fa-star"></i>';
+    let allCards = document.querySelectorAll(".card");
+    for (y = 0; y < allCards.length; y++) {
+        let everyCard = allCards[y];
+        let changeCards = cardlist[y];
+        everyCard.className = "card";
+        everyCard.firstElementChild.remove();
+        if (changeCards === "diamondOne" || changeCards === "diamondTwo") {
+            everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-diamond"></i>');
+        } else if (changeCards === "planeOne" || changeCards === "planeTwo") {
+            everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-paper-plane-o"></i>');
+        } else if (changeCards === "anchorOne" || changeCards === "anchorTwo") {
+            everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-anchor"></i>');
+        } else if (changeCards === "boltOne" || changeCards === "boltTwo") {
+            everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-bolt"></i>');
+        } else if (changeCards === "cubeOne" || changeCards === "cubeTwo") {
+            everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-cube"></i>');
+        } else if (changeCards === "leafOne" || changeCards === "leafTwo") {
+            everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-leaf"></i>');
+        } else if (changeCards === "bycicleOne" || changeCards === "bycicleTwo") {
+            everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-bicycle"></i>');
+        } else if (changeCards === "bombOne" || changeCards === "bombTwo") {
+            everyCard.insertAdjacentHTML('afterBegin', '<i class="fa fa-bomb"></i>');
+        } else {};
+    }
+}
+löschen();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -91,14 +84,14 @@ The <li> is pushed into the Array flippedCards to flip both cards back or let bo
 Invokes two next Funtions openCards to check for matches and Starcount.
 */
 function showCard(evt) {
-  if (evt.target.nodeName === "LI" && evt.target.className !== "card match")
-  {evt.target.className = "card open show";
-  openCardsList.push(evt.target.firstElementChild.className);
-  flippedCards.push(evt.target);
-  evt.target.style.cssText = "transform: rotateY(180deg); transition: 0.4s;";
-}
-  openCards(evt);
-  Starcount();
+    if (evt.target.nodeName === "LI" && evt.target.className !== "card match") {
+        evt.target.className = "card open show";
+        openCardsList.push(evt.target.firstElementChild.className);
+        flippedCards.push(evt.target);
+        evt.target.style.cssText = "transform: rotateY(180deg); transition: 0.4s;";
+    }
+    openCards(evt);
+    Starcount();
 }
 
 /*Checks if there are 2 cards flipped and if they match. For a match Function cardmatch is triggered.
@@ -109,16 +102,15 @@ The moves Function counts the moves used to complete the Game
 */
 
 function openCards(evt) {
-  if (openCardsList.length === 2 && openCardsList[0] === openCardsList[1]) {
-matchedCards.push("match");
-cardmatch();
-openCardsList.splice(0,2);
-moves();
-  }
-  else if (openCardsList.length === 2 && openCardsList[0] !== openCardsList[1]) {
-setTimeout(flipback,500);
-openCardsList.splice(0,2);
-moves();
+    if (openCardsList.length === 2 && openCardsList[0] === openCardsList[1]) {
+        matchedCards.push("match");
+        cardmatch();
+        openCardsList.splice(0, 2);
+        moves();
+    } else if (openCardsList.length === 2 && openCardsList[0] !== openCardsList[1]) {
+        setTimeout(flipback, 500);
+        openCardsList.splice(0, 2);
+        moves();
     };
 }
 
@@ -127,38 +119,38 @@ moves();
 function flipback() {
     flippedCards[0].className = "card";
     flippedCards[1].className = "card";
-    flippedCards.splice(0,2);
-  }
+    flippedCards.splice(0, 2);
+}
 
 /*Changes the class of the matched cards and deletes the values in the Array. Triggers the finish game Function with a delay to see if the game is finished.*/
 
-  function cardmatch() {
-      flippedCards[0].className = "card match";
-      flippedCards[1].className = "card match";
-      flippedCards.splice(0,2);
-      setTimeout(finishGame, 300);
-    }
+function cardmatch() {
+    flippedCards[0].className = "card match";
+    flippedCards[1].className = "card match";
+    flippedCards.splice(0, 2);
+    setTimeout(finishGame, 300);
+}
 
 /*Funtion to finish the Game. Checks if there are 8 matched Cards in the Array. Deletes all the Values in the Array at the End. Clears the Time function for a new Start.
 Alert to show moves, time and Message. Button to restart the game*/
 
 function finishGame() {
-  if (matchedCards.length === 8) {
-    swal({
-      title:"Congratulations You Won! ",
-      text:"With " + movesUsed + " moves and " + Sterne + " Stars! You needed " + timeUsed[0] + " seconds",
-      icon: "success",
-      button: {
-        text: "restart",
-        value: "start",
-      },
-    })
-.then(value => {
-  if (value) throw löschen();
-})
-clearTimeout(myVar);
-matchedCards.splice(0,8);
-};
+    if (matchedCards.length === 8) {
+        swal({
+                title: "Congratulations You Won! ",
+                text: "With " + movesUsed + " moves and " + Sterne + " Stars! You needed " + timeUsed[0] + " seconds",
+                icon: "success",
+                button: {
+                    text: "restart",
+                    value: "start",
+                },
+            })
+            .then(value => {
+                if (value) throw löschen();
+            })
+        clearTimeout(myVar);
+        matchedCards.splice(0, 8);
+    };
 };
 
 /*If the repeat Symbol is clicked the löschen funtion will be triggered*/
@@ -169,7 +161,7 @@ repeat.addEventListener("click", löschen);
 /*Count the moves. Increases the moves when there is a match or missmatch.*/
 
 function moves() {
-  var movecount = document.querySelector('.moves');
+    var movecount = document.querySelector('.moves');
     movecount.innerText++;
     movesUsed = movecount.innerText;
 }
@@ -178,30 +170,34 @@ function moves() {
 
 var stars = document.querySelector('.stars');
 var Sterne = "3";
+
 function Starcount() {
-      if (movesUsed == 13) {
+    if (movesUsed == 13) {
         stars.children[0].innerHTML = '<i class="fa fa-star-o"></i>';
         Sterne = "2";
-      }
-      else if (movesUsed == 17) {
+    } else if (movesUsed == 17) {
         stars.children[1].innerHTML = '<i class="fa fa-star-o"></i>';
         Sterne = "1";
-        }
-      else if (movesUsed == 21) {
+    } else if (movesUsed == 21) {
         stars.children[2].innerHTML = '<i class="fa fa-star-o"></i>';
         Sterne = "0";
-      }
     }
+}
 
-    /*Timer Funtion Definition. Counts Seconds after the first Click. Deletes the EventListener at the End so the time is not sped up with multiple Time funtions stacking. */
+/*Timer Funtion Definition. Counts Seconds after the first Click. Deletes the EventListener at the End so the time is not sped up with multiple Time funtions stacking. */
 
-    var myVar;
-    var timeUsed = [];
-    function time() {
+var myVar;
+var timeUsed = [];
+
+function time() {
     let seconds = "0";
-    myVar = setInterval(function() {seconds++; document.getElementById("time").innerHTML = seconds + "s";timeUsed.splice(0,1,seconds);} ,1000);
+    myVar = setInterval(function() {
+        seconds++;
+        document.getElementById("time").innerHTML = seconds + "s";
+        timeUsed.splice(0, 1, seconds);
+    }, 1000);
     this.removeEventListener("click", time);
-    }
+}
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
